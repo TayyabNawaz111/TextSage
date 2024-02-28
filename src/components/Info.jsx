1558b2825ac3d52f1cf032ee7a901d59f9951741
Muhammad Tayyab Nawaz <m.tayyab33600@gmail.com>
2024-02-28 12:00:58 +0500

    created info.jsx component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,18 @@ import React from "react";
 function Info() {
   return (
     <>
-      <div> HELLO WORLD</div>;
+      <div>
+        {" "}
+        <p className="text-center font-extrabold text-4xl mt-16">
+          54% OF CUSTOMERS WOULD <br /> LOVE TO GET PROMOTIONS SENT <br /> VIA
+          SMS
+        </p>
+        <p className="text-center text-purple-700 mt-10 text-2xl font-semibold ">
+          Combine automated texts with email and push notifications into a
+          single automated platform
+        </p>
+      </div>
+      ;
     </>
   );
 }
