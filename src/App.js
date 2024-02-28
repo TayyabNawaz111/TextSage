c99d204884ffce7187c078d1d7c7597cf5d1727e
Muhammad Tayyab Nawaz <m.tayyab33600@gmail.com>
2024-02-28 13:19:59 +0500

    Cards added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import "./App.css";
+import React from "react";
 import NavBar from "./components/NavBar";
 import Brief from "./components/Brief";
 import Info from "./components/Info";
+import Card from "./components/Card";
+
 function App() {
   return (
     <>
-      <NavBar></NavBar>
-      <Brief></Brief>
-      <Info></Info>
+      <NavBar />
+      <Brief />
+      <Info />
+      <div className="flex">
+        <Card
+          bg="gray"
+          header="SMS FOR CAMPAIGNS"
+          content="HASSLE FREE SMS CAMPAIGNS"
+          opacity="700"
+        />
+        <Card
+          bg="purple"
+          header="Simplify"
+          content="ANALYTICS AT YOUR HANDS"
+          opacity="500"
+        />
+        <Card
+          bg="gray"
+          header="Collaboration for all"
+          content="TRACK THE CUSTOMERS"
+          opacity="500"
+        />
+        <Card
+          bg="blue"
+          header="INTEGRATION"
+          content="TRANSPARENT PRICING"
+          opacity="500"
+        />
+      </div>
     </>
   );
 }
